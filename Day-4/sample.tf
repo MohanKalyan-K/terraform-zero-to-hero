7162404111a3691360687c9a49ebc217a6e33894
MohanKalyan-K <nishanthkalyan68@gmail.com>
2024-08-18 22:01:31 +0530

    Update sample.tf

--- Day-4/sample.tf
+++ Day-4/sample.tf
@@ -1,3 +1,9 @@
 provider "aws" {
           region = "us-east-1"
 }
+resource "aws_instance" "exmple" {
+          ami = "ami-0ae8f15ae66fe8cda"
+          instance_type = "t2.micro"
+}
+          
+          
